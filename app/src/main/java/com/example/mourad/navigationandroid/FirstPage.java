3ef2537c3d77d03955339ea6357f08895756a4a5
Mouradgb <Mouradbenamar12@gmail.com>
2018-02-03 12:35:32 +0000

    intent

--- app/src/main/java/com/example/mourad/navigationandroid/FirstPage.java
+++ app/src/main/java/com/example/mourad/navigationandroid/FirstPage.java
@@ -1,7 +1,6 @@
 package com.example.mourad.navigationandroid;
 
 import android.content.Intent;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.support.annotation.NonNull;
