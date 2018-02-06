ed21ff81e115f6aa7c143d56b9fd755803e9e838
Mouradgb <Mouradbenamar12@gmail.com>
2018-02-06 10:18:23 +0000

    Reset password

--- app/src/main/java/com/example/mourad/navigationandroid/ForgetPsw.java
+++ app/src/main/java/com/example/mourad/navigationandroid/ForgetPsw.java
@@ -1,13 +1,28 @@
 package com.example.mourad.navigationandroid;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
-public class ForgetPsw extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgetPsw extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText input_email;
+    private Button btnResetPass;
+    private RelativeLayout activity_forgot;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +31,14 @@ public class ForgetPsw extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_forget_psw);
+
+        input_email = (EditText)findViewById(R.id.frg_email);
+        btnResetPass = (Button)findViewById(R.id.reset_psw);
+        activity_forgot = (RelativeLayout)findViewById(R.id.activity_forgot_psw);
+
+        btnResetPass.setOnClickListener(this);
+
+        auth = FirebaseAuth.getInstance();
     }
     public void btnLogin(View view){
 
@@ -26,5 +49,33 @@ public class ForgetPsw extends AppCompatActivity {
 
         Intent myIntent=new Intent(this,SignUp.class);
         startActivity(myIntent);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.reset_psw)
+        {
+            resetPassword(input_email.getText().toString());
+        }
+    }
+
+    private void resetPassword(final String email) {
+        auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                        {
+                            Snackbar snackBar = Snackbar.make(activity_forgot,"We have sent password to email: "+email,Snackbar.LENGTH_SHORT);
+                            snackBar.show();
+                        }
+                        else{
+                            Snackbar snackBar = Snackbar.make(activity_forgot,"Failed to send password",Snackbar.LENGTH_SHORT);
+                            snackBar.show();
+                        }
+                    }
+                });
     }
 }
