005f7fee36e396091600447d6924b09a4cd1ab34
AbdelleBay <abdellebay@gmail.com>
2018-02-09 03:06:56 +0000

    fix verify done

--- app/src/main/java/com/example/mourad/navigationandroid/VerifyEmail.java
+++ app/src/main/java/com/example/mourad/navigationandroid/VerifyEmail.java
@@ -1,9 +1,14 @@
 package com.example.mourad.navigationandroid;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,25 +24,34 @@ import java.util.TimerTask;
 
 public class VerifyEmail extends AppCompatActivity {
 
-    Button btnSend,btnRefresh;
-    TextView txtEmail,txtStatus,et_email,login_email;
+    Button btnSend;
+    TextView txtEmail,et_email,login_email;
     Timer t;
-    boolean start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_email);
 
+        FirebaseAuth.getInstance().getCurrentUser().sendEmailVerification();
+
+        //Timer refresh
+        t= new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+
+                                  @Override
+                                  public void run() {
+                                      refresh();
+                                  }
+
+                              },
+                0,
+                3000);
+
         btnSend = findViewById(R.id.btn_send);
-        btnRefresh = findViewById(R.id.btn_refresh);
         txtEmail = findViewById(R.id.txt_email);
-        txtStatus = findViewById(R.id.txt_status);
         et_email = findViewById(R.id.et_email);
         login_email = findViewById(R.id.login_email);
-        t = new Timer();
         setInfo();
-        start=true;
-
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,21 +72,6 @@ public class VerifyEmail extends AppCompatActivity {
                                 }
                             }
                         });
-                if (start){
-                    start=false;
-                    t.scheduleAtFixedRate(new TimerTask() {
-
-                                              @Override
-                                              public void run() {
-                                                  refresh();
-                                              }
-
-                                          },
-                            0,
-                            3000);
-                }
-                start=false;
-
             }
         });
 
@@ -82,10 +81,10 @@ public class VerifyEmail extends AppCompatActivity {
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
-            txtEmail.setText(new StringBuilder("Email : ").append(user.getEmail()));
-        }
-        if (user != null) {
-            txtStatus.setText(new StringBuilder("Verify :").append(String.valueOf(user.isEmailVerified())));
+            String str1="A verification link was sent to ";
+            String str2=" when you first created this account. Please click on that link to verify your email address. if you cannot find it you may request a new one below";
+            String Email= "<font color='#EE0000'>"+ user.getEmail() +"</font>";
+            txtEmail.setText(Html.fromHtml(str1+Email+str2));
         }
 
     }
@@ -96,10 +95,10 @@ public class VerifyEmail extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                        if (user.isEmailVerified()){
+                        if (user != null && user.isEmailVerified()) {
                             t.cancel();
-                            Toast.makeText(getApplicationContext(),"your email "+" << "+user.getEmail()+" >> "+" is verified",Toast.LENGTH_LONG).show();
-                            Intent intent = new Intent(getApplicationContext(),Login.class);
+                            Toast.makeText(getApplicationContext(), "your email is verified", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(getApplicationContext(), Login.class);
                             startActivity(intent);
                             finish();
                         }
