9f9bd57acf2823e63755a3f9931e9fbf72caa8c8
Mouradgb <Mouradbenamar12@gmail.com>
2018-02-24 14:58:53 +0000

    maps

--- app/src/main/java/com/example/mourad/navigationandroid/OffreInformation.java
+++ app/src/main/java/com/example/mourad/navigationandroid/OffreInformation.java
@@ -1,19 +1,32 @@
 package com.example.mourad.navigationandroid;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class OffreInformation extends AppCompatActivity {
+public class OffreInformation extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offre_information);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
 
         TextView tvName,tvSource,tvDestination,tvDate,tvTime,tvPhone,tvCarid;
         ImageView imageWay;
@@ -48,6 +61,18 @@ public class OffreInformation extends AppCompatActivity {
 
         Glide.with(getApplicationContext()).load(image).into(imageWay);
 
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
     }
 }
