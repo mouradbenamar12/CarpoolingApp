d049702b62ea4d9a56eafd4af905d0bb98c830c0
Mouradgb <Mouradbenamar12@gmail.com>
2018-03-07 20:36:45 +0000

    Share, about and Call Phone !!

--- app/src/main/java/com/example/mourad/navigationandroid/HomeFragment.java
+++ app/src/main/java/com/example/mourad/navigationandroid/HomeFragment.java
@@ -12,23 +12,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,7 +32,7 @@ public class HomeFragment extends Fragment {
     RecyclerView recyclerView;
     ProgressDialog progress;
     List<Rider_Ways> list = new ArrayList<>();
-    DatabaseReference myRef,Users,uid,favorite;
+    DatabaseReference myRef;
     RecyclerView.Adapter adapter ;
 
     ImageView fav_image;
