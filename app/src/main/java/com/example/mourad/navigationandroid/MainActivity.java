8f84a08720642f7442b25df0119b9e114b24424b
Mouradgb <Mouradbenamar12@gmail.com>
2018-03-13 15:28:15 +0000

    Animation 1

--- app/src/main/java/com/example/mourad/navigationandroid/MainActivity.java
+++ app/src/main/java/com/example/mourad/navigationandroid/MainActivity.java
@@ -120,13 +120,8 @@ public class MainActivity extends BaseActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            showProgressDialog();
-            signOut();
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
